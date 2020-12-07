a4035d0900068ba5fd579b1d9b345390d41b695a
James Francis <jfrancis@companieshouse.gov.uk>
2020-12-07 10:27:49 +0000

    New version - new journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Insolvency enablement',
+  serviceName: 'Service name here',
 
   // Default port that prototype runs on
   port: '3000',

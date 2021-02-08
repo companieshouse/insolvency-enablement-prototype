323314d2771547b47386b432c3e8ca2f0164fc2b
Jason <jdeakin@companieshouse.gov.uk>
2021-02-08 08:40:22 +0000

    Separating form into separate pages and adding validation errors wip

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,17 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+module.exports = router
+router.get('/', function (req, res) {
+  req.session.scenario = {}
+  res.render('index')
+})
+
+router.get('/limited-company', function (req, res) {
+    req.session.scenario = {}
+    res.render('limited-company')
+  })
+
+  router.post('/form', function (req, res) {
+    res.redirect('/form/applicant-name')
+  })
